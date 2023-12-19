cb514d79dbc2c900764ee354f394f4d27af3d81e
wildgolem <chhun.goldlen@courrier.uqam.ca>
2023-12-18 20:54:38 -0500

    add catch error to commands

--- commands/utilities/purge.js
+++ commands/utilities/purge.js
@@ -5,7 +5,8 @@ module.exports = {
 		.setName('purge')
 		.setDescription('Deletes last 100 messages.'),
 	async execute(interaction) {
-		await interaction.channel.bulkDelete(100);
-        await interaction.reply('Messages deleted');
+		await interaction.channel.bulkDelete(100).then(() => {
+			interaction.reply('Messages deleted').catch(console.error);
+		});
 	},
 };
