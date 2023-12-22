ef5530be8338bae284459e6c4acb15fb6c345745
wildgolem <chhun.goldlen@courrier.uqam.ca>
2023-12-22 15:34:42 -0500

    add animepahe logo

--- animepahe/feed.js
+++ animepahe/feed.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 function gogoanime() {
     require('dotenv').config();
     const { EmbedBuilder, WebhookClient } = require('discord.js');
@@ -12,17 +15,32 @@ function gogoanime() {
         refresh: 300000,
     });
 
-    feeder.on('new-item', function(item) {
+    feeder.on('new-item', async function(item) {
         const embed = new EmbedBuilder()
-            .setColor(0xFF00FF)
-            .setTitle(`${item.title}`)
-            .setURL(`${item.link}`)
-            .setImage(`${item.description}`);
+            .setAuthor({
+                name: item.title,
+                iconURL: `${await searchImage(item)}`,
+                url: item.link
+            })
+            .setColor(0xD5015B)
+            .setImage(`${item.description}`)
+            .setTimestamp();
 
         webhook.send({ embeds: [embed] });
     });
 
     feeder.on('error', console.error);
+}
+
+async function searchImage(item) {
+    try {
+        const searchPage = await axios.get(`https://gogoanime3.net/search.html?keyword=${item.title.split(" ").slice(0, -3).join(" ")}&page=1`);
+        const $ = cheerio.load(searchPage.data);
+        return $('div.last_episodes > ul > li > div > a > img').attr('src');
+       
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = { gogoanime };
--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		client.user.setAvatar('./images/andre.png');
+		//client.user.setAvatar('./images/andre.png');
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
